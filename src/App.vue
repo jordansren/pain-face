69c3a6d8944a663f3e7bbe8897232a2591c76662
jordan_ren <jordan.ren@kitware.com>
2020-05-22 17:37:32 -0400

    Changed format of input, mostly working

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,80 @@
 <template>
   <v-app id="app">
-    <!-- Title -->
-    <v-container>
-      <v-layout row wrap>
-        <v-flex xs6 md3>
-          <h1 class="display-2">aMGS <br> Upload Tool </h1>
-        </v-flex>
-        <v-flex xs6 md9>
-          <v-img class="mainImage center" src="./assets/Mouse.jpeg" alt="Mouse"></v-img>
-        </v-flex>
-      </v-layout>
-      <v-divider></v-divider>
-      <!-- Main Content and Uploader -->
-      <v-layout row wrap>
-        <!-- Drag and drop area -->
-        <v-layout column>
-          <v-flex xs6 md3 class="area">
-            <label>
-              <h3>Click or Drag to choose file</h3>
-            </label>
-            <v-img class="icon center" src="./assets/upload.png" alt="upload"></v-img>
-            <input id="upload" type="file" accept="image/jpeg,image/png" @change="onImageChosen">
-          </v-flex>
-        </v-layout>
-        <!-- Preview and upload -->
-        <v-layout column>
-          <v-flex xs6 md9>
-            <p v-if="wrongImageType">
-              Oops! You did not input an image of type png, jpeg, or jpg. <br> Current file to upload: {{ images[0].name }}
-            </p>
-            <v-btn class="primary" @click="onImageUpload" v-if="!uploadingImage">
-              <p v-if="numberOfImages > 1">Upload Images</p>
-              <p v-else>Upload Image</p>
-            </v-btn>
-          </v-flex>
-          <v-flex xs6 md9>
-            <div class="image-preview" v-if="imageData.length > 0">
-              <div v-for="img in imageData" :key="img">
-                <img class="preview imageStack" :src="img">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12" style="width:500px">
+              <h1 class="display-2" style="text-align: center;">AMGS Upload Tool </h1>
+              <v-img class="mainImage center" src="./assets/Mouse.jpeg" alt="Mouse"></v-img>
+            </v-card>
+            <v-container grid-list-md></v-container>
+            <v-card class="elevation-12 inputcard">
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title>Upload File</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                    </v-btn>
+                  </template>
+                </v-tooltip>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn class="primary" style="text-align: center" @click="onUpload" v-if="!uploadingFile">
+                    <p style="top:10px">Upload File</p>
+                  </v-btn>
+                </v-card-actions> 
+              </v-toolbar>
+              <div v-if="!uploadingFile">
+                <div class="area">
+                  <v-img class="icon center" src="./assets/upload.png" alt="upload"></v-img>
+                  <input id="upload" type="file" accept="image/jpeg,image/png,image/jpg,video/mp4" @change="onFileChosen">
+                </div>
+                <div class="preview center">
+                  <div class="preview center" v-if="fileType=='image'">
+                    <img class="preview imageStack center" :src="imageData">
+                  </div>
+                  <video class="preview center" :src="blobURL" v-if="fileType=='video'"></video>
+                  <p style="text-align: center" v-if="wrongFileType">
+                    Oops! You did not input an file of type png, jpeg, jpg, or mp4. <br> Current file to upload: {{ file.name }}
+                  </p>
+                </div>
               </div>
-            </div>
-          </v-flex>
-          <div class="clear" v-if="uploadingImage">
-            <h3 v-if="numberOfImages > 1">Processing {{ images[0].name }}...</h3>
-            <h3 v-else>Processing {{ images[0].name }}...</h3>
-            <div class="loader"></div>
-          </div>
-        </v-layout>
-      </v-layout>
-    </v-container>
-  
-    <!-- Video Upload -->
-    <!-- <form>
-      <h3 >Videos</h3>
-      <input type="file" accept="video/*" @change="onVideoChosen" v-if="!uploadingVideo"><br>
-      <p v-if="wrongVideoType">
-        Oops! You did not input an image of type png, jpeg, or jpg. <br> Current file to upload: {{ videos[0].name }}
-      </p>
-      <video style="width:300px" :src="blobURL" v-if="numberOfVideos > 0"></video>
-      <button class="clear" @click="onVideoUpload" v-if="!uploadingVideo">
-        <p v-if="numberOfVideos > 1">Upload Videos</p>
-        <p v-else>Upload Video</p>
-      </button>
-      <div class="clear" v-if="uploadingVideo">
-        <h3 v-if="numberOfVideos > 1">Processing {{ videos[0].name }}...</h3>
-        <h3 v-else>Processing {{ videos[0].name }}...</h3>
-        <div class="loader"></div>
-      </div>
-    </form> -->
+              <div v-else>
+                <div class="clear center" v-if="uploadingFile">
+                  <h3 style="text-align:center">Processing {{ file.name }}...</h3>
+                  <v-progress-linear
+                    indeterminate
+                    color="green"
+                  ></v-progress-linear>
+                </div>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -75,29 +82,19 @@
   export default {
     data() {
       return {
-        images: [],
-        videos: [],
-        imageData: [],
-        uploadingImage: false,
-        uploadingVideo: false,
+        file: null,
+        imageData: null,
+        blobURL: null,
+        fileType: null,
+        uploadingFile: false,
         outputImage: null,
         outputVideo: null,
-        wrongImageType: false,
-        wrongVideoType: false,
-        blobURL: null
-      }
-    },
-    computed: {
-      numberOfImages() {
-        return this.images.length;
-      },
-      numberOfVideos() {
-        return this.videos.length;
+        wrongFileType: false
       }
     },
     methods: {
-      onImageChosen(event) {
-        /* Loop over files, check for only images */
+      onFileChosen(event) {
+        /* Loop over files, check for correct file types*/
         Array
           .from(Array(event.target.files.length).keys())
           .map(x => {
@@ -105,117 +102,74 @@
             if (tag == "png" || tag == "jpeg" || tag == "jpg") {
               /* Since we are only doing single files for now, this if statement
                   will ensure only one file is stored at a time */
-              if (this.images.length == 1) {
-                this.images = [];
-                this.imageData = [];
-                this.wrongImageType = false;
+              if (this.file != null) {
+                this.file = null;
+                this.imageData = null;
               }
               this.showImage(event, x)
-              this.images.push(event.target.files[x]);
-            } else {
-              this.wrongImageType = true;
-            }
-          });
-      },
-      onVideoChosen(event) {
-        /* Loop over files, check for only videos */
-        Array
-          .from(Array(event.target.files.length).keys())
-          .map(x => {
-            let tag = event.target.files[x].name.split(".")[1];
-            if (tag == "mp4") {
-              /* Since we are only doing single files for now, this if statement
-                  will ensure only one file is stored at a time */
-              if (this.videos.length == 1) {
-                this.videos = [];
-                this.videoData = [];
-                this.wrongVideoType = false;
-              } 
+              this.wrongFileType = false;
+              this.fileType = "image";
+              this.file = event.target.files[x];
+            } else if (tag == "mp4") {
+              if (this.file != null) {
+                this.file = null;
+                this.blobURL = null;
+              }
+              this.wrongFileType = false;
               this.showVideo(event);
-              this.videos.push(event.target.files[x]);
+              this.fileType = "video";
+              this.file = event.target.files[x];
             } else {
-              this.wrongVideoType = true;
+              this.wrongFileType = true;
             }
           });
       },
-      async onImageUpload() {
-        this.uploadingImage = true;
+      async onUpload() {
+        let tag = this.file.name.split(".")[1];
+        this.uploadingFile = true;
         const axios = require('axios');
-        const path = 'http://localhost:8080/'; /* Not sure what to put here */
-        this.outputImage = await axios.get(path);
-        /* this.uploadingImage = false; */
+        if (tag == "mp4") {
+          const path = 'http://localhost:8080/vidUpload'; /* Not sure what to put here */
+          this.outputImage = await axios.get(path);
+        } else {
+          const path = 'http://localhost:8080/imgUpload'; /* Not sure what to put here */
+          this.outputVideo = await axios.get(path);
+        }
+        /* this.reset(); */
       },
-      async onVideoUpload() {
-        this.uploadingVideo = true;
-        const axios = require('axios');
-        const path = 'http://localhost:8080/'; /* Not sure what to put here */
-        this.outputVideo = await axios.get(path);
-        /* this.uploadingVideo = false; */
-      },
-      showImage(event, i) {
+      showImage(event) {
         var input = event.target;
-        if (input.files && input.files[i]) {
+        if (input.files && input.files[0]) {
           var reader = new FileReader();
           reader.onload = (e) => {
-              this.imageData.push(e.target.result);
+              this.imageData = e.target.result;
           }
-          reader.readAsDataURL(input.files[i]);
+          reader.readAsDataURL(input.files[0]);
         }
       },
       showVideo(event) {
         let file = event.target.files[0];
         this.blobURL = URL.createObjectURL(file);
       },
-      reset(fileType) {
-        if (fileType == "image") {
-          this.images = [];
-        } 
-        if (fileType == "video") {
-          this.videos = []; 
-        }
+      reset() {
+        this.file = null;
+        this.imageData = null;
+        this.blobURL = null;
+        this.fileType = null;
+        this.uploadingFile = false;
+        this.outputImage = null;
+        this.outputVideo = null;
+        this.wrongFileType = false;
       }
     }
   }
 </script>
 
 <style>
-  .split {
-    height: 100%;
-    width: 50%;
-    position: fixed;
-    z-index: 1;
-    top: 200;
-    overflow-x: hidden;
-    padding-top: 20px;
+  body{
     background-color: rgb(149, 214, 240);
-  } 
-
-  .left {
-    left: 0;
-    top: 200;
-    }
-
-  .right {
-    right: 0;
-    top: 200;
-  }
-    body{
-      background-color: rgb(149, 214, 240);
-    }
-
-  .loader {
-    border: 16px solid #f3f3f3; /* Light grey */
-    border-top: 16px solid #3498db; /* Blue */
-    border-radius: 50%;
-    width: 120px;
-    height: 120px;
-    animation: spin 2s linear infinite;
   }
 
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
 
   img.preview {
     width: 300px;
@@ -242,17 +196,16 @@
   }
 
   .center {
-    display: block;
+    display: inline-block;
     margin-left: auto;
     margin-right: auto;
   }
 
   .area {
-    width: 20%;
-    height: 20%;
+    width: 100%;
+    height: 200px;
     position: absolute;
     border: 4px dashed #000;
-    background-image: url("http://kmtlondon.com/img/upload.png");
     background-position: center;
     background-repeat: no-repeat;
     background-size: 64px 64px;
@@ -264,6 +217,7 @@
     -moz-opacity: 0.5;
     opacity: 0.5;
     text-align: center;
+    background-color: aqua;
   }
 
   .area:hover,
@@ -276,20 +230,34 @@
   }
 
   .area input {
-    width: 400%;
-    height: 100%;
-    margin-left: -300%;
+    width: 500px;  
+    height: 200px;
     border: none;
     cursor: pointer;
+    opacity: 0;
+    z-index: 1;
   }
 
   .area input:focus {
       outline: none;
   }
 
+  .inputcard {
+    width: 500px;
+    height: 500px;
+  }
+
+  .preview {
+    top: 200px;
+    max-height: 200px;
+    max-width: 400px; 
+    text-align: center;
+  }
+
   .icon {
-    top: 20px;
-    width: 40px;
-    height: 40px;
+    top: 60px;
+    width: 60px;
+    height: 60px;
+    z-index: 2;
   }
 </style>
