5bdc0b968afdb10addc357fb8c4632c5927470c0
jordan_ren <jordan.ren@kitware.com>
2020-06-02 15:30:36 -0400

    Working preUpload

--- client/src/components/upload.vue
+++ client/src/components/upload.vue
@@ -1,8 +1,6 @@
 <template>
   <v-content>
-    <v-card
-      class="elevation-12"
-    >
+    <v-card class="elevation-12">
       <!-- Tool Bar -->
       <v-toolbar
         color="primary"
@@ -20,6 +18,7 @@
         :post-upload="onUpload"
         :multiple="false"
         :accept="accepted"
+        :pre-upload="createFolder"
       />
     </v-card>
   </v-content>
@@ -44,7 +43,6 @@ export default {
         _id: null,
         _modelType: null,
       },
-      /* PROBLEM WITH ACCEPTING ONLY TYPES BELOW */
       accepted: 'image/jpeg,image/jpg,image/png,video/mp4',
       user: this.girderRest.user.login,
     };
@@ -54,27 +52,27 @@ export default {
       return !(this.girderRest.user == null);
     },
   },
-  async mounted() {
-    if (this.girderRest.user == null) {
-      this.$router.push('/');
-    }
-    const res = await this.girderRest.post('folder', stringify({
-      parentType: 'user',
-      parentId: this.girderRest.user._id,
-      name: `Mouse Data ${new Date().toISOString()}`,
-    }));
-    this.dest = res.data;
-    return this.dest;
-  },
   methods: {
     async onUpload() {
-      const res = await this.girderRest.post(`mouse_pain_face/${this.dest._id}`);
+      const res = await this.girderRest.post(`mouse_pain_face/${this.dest.dest._id}`);
       this.$router.push({
         name: 'Result',
         params: {
           data: res.data,
         },
       });
+    },
+    async createFolder() {
+      if (this.girderRest.user == null) {
+        this.$router.push('/');
+      }
+      const res = await this.girderRest.post('folder', stringify({
+        parentType: 'user',
+        parentId: this.girderRest.user._id,
+        name: `Mouse Data ${new Date().toISOString()}`,
+      }));
+      this.dest = { dest: res.data };
+      return this.dest;
     },
   },
 };
