082a0fb518cdee65efa4805853dbbf5cdbea00ec
jordan_ren <jordan.ren@kitware.com>
2020-06-03 09:41:51 -0400

    First version of PWA working

--- client/src/App.vue
+++ client/src/App.vue
@@ -38,7 +38,19 @@
 
 <script>
 export default {
-
+  methods: {
+    async registerSW() {
+      if ('serviceWorker' in navigator) {
+        try {
+          await navigator.serviceWorker.register('./sw.js');
+        } catch (e) {
+          alert('ServiceWorker registration failed. Sorry about that.');
+        }
+      } else {
+        document.querySelector('.alert').removeAttribute('hidden');
+      }
+    },
+  },
 };
 </script>
 <style scoped>
